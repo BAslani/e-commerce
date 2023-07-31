55901b4cb6f0c2d6178418611b29cb8332020289
Behzad Aslani <behzadaslani80@gmail.com>
2023-08-01 02:35:13 +0330

    add to cart functionality

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -10,7 +10,61 @@ import { findProduct } from '../store.js';
 import addToCartDOM from './addToCartDOM.js';
 // set items
 
+const cartItemCountDOM = getElement('.cart-item-count');
+const cartItemsDOM = getElement('.cart-items');
+const cartTotalDOM = getElement('.cart-total');
+
+let cart = getStorageItem('cart');
+
+
 export const addToCart = (id) => {
-  console.log(id);
+  let item = cart.find((cartItem) => cartItem.id === id);
+  if (!item) {
+    let product = findProduct(id);
+    // adding item to the cart
+    product = { ...product, amount: 1 };
+    cart = [...cart, product]
+    // adding item to the DOM
+    addToCartDOM(product)
+  } else {
+
+  }
+  // update items count
+  displayCartItemsCount()
+  // display cart totals
+  displayCartTotal()
+  // calculate total
+
+  // set cart in the local storage
+  setStorageItem('cart', cart)
   openCart()
 };
+
+function displayCartItemsCount() {
+  const amount = cart.reduce((total, cartItem) => {
+    return total += cartItem.amount;
+  }, 0)
+  cartItemCountDOM.textContent = amount;
+}
+function displayCartTotal() {
+  let total = cart.reduce((total, cartItem) => {
+    return total += cartItem.price * cartItem.amount;
+  }, 0)
+  cartTotalDOM.textContent = `Total : ${formatPrice(total)}`
+}
+
+function displayCartItemsDOM(){
+  cart.forEach((cartItem)=> addToCartDOM(cartItem))
+}
+function setupCartFunctionality(){
+
+}
+
+const init = () => {
+  displayCartItemsCount();
+  displayCartTotal();
+  displayCartItemsDOM();
+  setupCartFunctionality();
+}
+
+init()
