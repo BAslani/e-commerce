55df7a5badf55d14851d558e267bc946360aeaf0
Behzad Aslani <behzadaslani80@gmail.com>
2023-08-01 03:41:21 +0330

    finnished project

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -27,7 +27,10 @@ export const addToCart = (id) => {
     // adding item to the DOM
     addToCartDOM(product)
   } else {
-
+    const amount = increaseAmount(id)
+    const items = [...cartItemsDOM.querySelectorAll('.cart-item-amount')];
+    const newAmount = items.find((value) => value.dataset.id === id);
+    newAmount.textContent = amount;
   }
   // update items count
   displayCartItemsCount()
@@ -39,6 +42,32 @@ export const addToCart = (id) => {
   setStorageItem('cart', cart)
   openCart()
 };
+function removeItem(id){
+  cart = cart.filter((cartItem) => cartItem.id !== id);
+}
+
+function increaseAmount(id){
+  let newAmount;
+  cart = cart.map((cartItem) => {
+    if(cartItem.id === id){
+      newAmount = cartItem.amount + 1
+      cartItem = {...cartItem, amount: newAmount}
+    }
+    return cartItem
+  })
+  return newAmount;
+}
+function decreaseAmount(id){
+  let newAmount;
+  cart = cart.map((cartItem) => {
+    if(cartItem.id === id){
+      newAmount = cartItem.amount - 1
+      cartItem = {...cartItem, amount: newAmount}
+    }
+    return cartItem
+  })
+  return newAmount;
+}
 
 function displayCartItemsCount() {
   const amount = cart.reduce((total, cartItem) => {
@@ -57,7 +86,36 @@ function displayCartItemsDOM(){
   cart.forEach((cartItem)=> addToCartDOM(cartItem))
 }
 function setupCartFunctionality(){
+  cartItemsDOM.addEventListener('click', function(e){
+    const element = e.target;
+    const parent = e.target.parentElement;
+    const id = e.target.dataset.id;
+    const parentID = e.target.parentElement.dataset.id;
 
+    // remove
+    if(element.classList.contains('cart-item-remove-btn')){
+      removeItem(id);
+      parent.parentElement.remove();
+    }
+    // increase
+    if(parent.classList.contains('cart-item-increase-btn')){
+      const newAmount = increaseAmount(parentID);
+      parent.nextElementSibling.textContent = newAmount;
+    }
+    // decrease
+    if(parent.classList.contains('cart-item-decrease-btn')){
+      const newAmount = decreaseAmount(parentID);
+      if (newAmount === 0){
+        removeItem(parentID);
+        parent.parentElement.parentElement.remove();
+      }else{
+        parent.previousElementSibling.textContent = newAmount;
+      }
+    }
+    displayCartItemsCount();
+    displayCartTotal();
+    setStorageItem('cart', cart);
+  })
 }
 
 const init = () => {
